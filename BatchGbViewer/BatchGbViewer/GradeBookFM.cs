41766806ed7425fe2bba587d0431036bb6bd4bb5
brandon.parnoff <brandon.parnoff@outlook.com>
2017-04-14 11:20:07 -0400

    attaching client API

--- BatchGbViewer/BatchGbViewer/GradeBookFM.cs
+++ BatchGbViewer/BatchGbViewer/GradeBookFM.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -16,6 +17,10 @@ namespace BatchGbViewer
    /// </summary>
    public partial class MainWindow : Window
    {
+      private static HttpClient client = new HttpClient();
+      private static HttpClient users = new HttpClient();
+      private static HttpClient exams = new HttpClient();
+
       /// <summary>
       /// This method will handle the events that occure when the user changes the listbox selection
       /// </summary>
@@ -24,6 +29,37 @@ namespace BatchGbViewer
       private void GB_listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
       {
 
+      }
+
+
+
+      private async Task<List<String>> GetBatches()
+      {
+         client.BaseAddress = new Uri("http://ec2-54-215-138-178.us-west-1.compute.amazonaws.com/UserBuffetService/");
+         client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+         HttpResponseMessage response = await client.GetAsync("./api/Batches/GetBatches");
+         response.EnsureSuccessStatusCode(); // throw an error code
+         var batches = await response.Content.ReadAsAsync<IEnumerable<Batch>>();
+      }
+      /// <summary>
+      /// This method will handle how the drop down filter is populated
+      /// </summary>
+      /// <param name="sender"></param>
+      /// <param name="e"></param>
+      private static async void GB_listBox_Loaded(object sender, RoutedEventArgs e)
+      {
+         try
+         {
+            using (HttpClient client = new HttpClient())
+            {
+               
+            }
+         }
+         catch (Exception ex)
+         {
+            Console.WriteLine(ex.ToString());
+         }
       }
 
       /// <summary>
@@ -61,17 +97,7 @@ namespace BatchGbViewer
       /// <param name="uri"></param>
       private void GetRESTData(string uri)
       {
-         using (var httpClient = new HttpClient())
-         {
-            try
-            {
-
-            }
-            catch (Exception ex)
-            {
-               MessageBox.Show(ex.Message);
-            }
-         }
+        
       }
    }
 }
